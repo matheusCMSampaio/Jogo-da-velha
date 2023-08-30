09fc6cfe335aefa94e6ee9ac404c0d62a8fb19b8
matheuscms <matheuschagas415@gmail.com>
2023-08-29 23:59:01 -0300

    Concluindo Jogador x jogador

--- jogo_velha.py
+++ jogo_velha.py
@@ -7,13 +7,13 @@ def main():
 def leiaCoordenadaLinha():
     aux = []
     for i in range(len(jogo)):
-        aux.append(i+1)
+        aux.append(i)
     return aux
     
 def leiaCoordenadaColuna():
     aux = []
     for i in range(len(jogo)):
-        aux.append(i+1)
+        aux.append(i)
     return aux
 
     
@@ -32,9 +32,10 @@ def imprimeMenuPrincipal():
     print('\tMenu principal\n')
     print('1 - J x J \n2 - J x Bot (Modo Fácil) \n3 - J x Bot (Modo difícil)\n0 - Sair')
     escolha = int(input('Escolha a opção: '))
+    time.sleep(2)
     match escolha:
         case 1:
-            imprimirTabuleiro()
+            modoJogador()
         case 2:
             print('2')
         case 3:
@@ -46,6 +47,82 @@ def imprimeMenuPrincipal():
             time.sleep(2)
             imprimeMenuPrincipal()
     
+def modoJogador():
+    os.system('cls')
+    aux = 1  
+    jogador = 1
+    linha = 0
+    coluna = 0
+    while aux <=9:
+        imprimirTabuleiro()
+        if aux % 2 == 0: #Verifica qual jogador irá jogar, ou seja, em numeros pares será o jogador 2 que joga
+            jogador = 2
+        else:
+            jogador = 1 # E impar é o jogador 1 que joga
+        linha= int(input(f'Jogador {jogador}: Escolha a linha: '))
+        coluna= int(input(f'Jogador {jogador}: Escolha a coluna: '))
+        if posicaoValida(linha, coluna):
+            if jogador == 1:
+                jogada = 'X'
+                jogo[linha][coluna] = 'X'
+            else:
+                jogada = 'O'
+                jogo[linha][coluna] = 'O'
+            
+            if validaColuna(jogada) or validaLinha(jogada) or validaVertical(jogada):
+                print(f'Jogador {jogador} vitórioso')
+                time.sleep(2)
+                os.system('cls')
+                escolha = input('Deseja continuar?')
+                if escolha== 'Sim' or escolha == 'sim':
+                    imprimeMenuPrincipal()
+                else:
+                    os.system('cls')
+                    time.sleep(2)
+                    print('Adeus')
+                    break
+
+        else:
+            print('\n\tPosição inválida')
+            aux =-1
+        aux += 1
+
+
+def posicaoValida(linha,coluna):
+    if jogo[linha][coluna] == ' ':
+        return True
+    else:
+        return False
+
+
+def verificaVencedor(jogador):
+    jogada = ''
+    if jogador == 1:
+        jogada = 'X'
+    else:
+        jogada = 'O'
+    
+def validaLinha(jogada):
+    for i in range(len(jogo)):
+        if jogo[i][0] == jogada and jogo[i][1] == jogada and jogo[i][2] == jogada:
+            return True
+    return False    
+
+def validaColuna(jogada):
+    for i in range(len(jogo)):
+        if jogo[0][i] == jogada and jogo[1][i] == jogada and jogo[2][i] == jogada:
+            return True
+    return False    
+
+def validaVertical(jogada):
+    if jogo[0][0] == jogada and jogo[1][1] == jogada and jogo[2][2] == jogada:
+        return True
+    elif jogo[0][2] == jogada and jogo[1][1] == jogada and jogo[2][0] == jogada:
+        return True
+    else:
+        return False
+
+
 
 jogo = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
 
